efff642ac52622145deb5c26d990a10df7e165dd
Roja <111202012900@mhs.dinus.ac.id>
2023-11-07 18:12:56 +0700

    UTS_BENGKOD_Poliklinik_v2.0_Roja

--- poliklinik.sql
+++ poliklinik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 27, 2023 at 10:26 AM
+-- Generation Time: Nov 07, 2023 at 12:09 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -39,12 +39,8 @@ CREATE TABLE `dokter` (
 --
 
 INSERT INTO `dokter` (`id_dokter`, `nama`, `alamat`, `no_hp`) VALUES
-(31, 'Roja', 'jalan merak, no 1 Rt.06 Rw.08 Penaruban, Weleri', '0895413421109'),
-(37, 'Roja', 'jalan merak, no 1 Rt.06 Rw.08 Penaruban, Weleri', '0895413421109'),
-(40, 'Raiden Ei', 'Inazuma Eleven', '77777777777'),
-(42, 'Ventod', 'Mondo', '1111111111111'),
-(43, 'Joni', 'Weleri', '12345678901'),
-(44, 'Harry Osborn', 'New York', '089890890980');
+(45, 'Joni Yes PAPA', 'jalan merak, no 1 Rt.06 Rw.08 Penaruban', '0897382738173837'),
+(46, 'Raiden Ei', 'Inazuma Eleven', '77777777778');
 
 -- --------------------------------------------------------
 
@@ -65,9 +61,10 @@ CREATE TABLE `pasien` (
 
 INSERT INTO `pasien` (`id_pasien`, `nama`, `alamat`, `no_hp`) VALUES
 (5, 'Roja', 'jalan merak', '0895413421109'),
-(10, 'Miles Morales', 'Harlem', '2190299101212'),
+(10, 'Miles Morales', 'Demak', '2190299101212'),
 (11, 'Gwen Stacy', 'Serumah sama Roja', '171721627126'),
-(12, 'Roja', 'jalan merak, no 1 Rt.06 Rw.08 Penaruban', '12345678901');
+(12, 'Roja', 'jalan merak, no 1 Rt.06 Rw.08 Penaruban', '12345678901'),
+(13, 'Peter', 'Inazuma', '085951465387');
 
 -- --------------------------------------------------------
 
@@ -89,9 +86,9 @@ CREATE TABLE `periksa` (
 --
 
 INSERT INTO `periksa` (`id`, `id_pasien`, `id_dokter`, `tgl_periksa`, `catatan`, `obat`) VALUES
-(13, 11, 42, '2023-10-28 03:11:00', 'Lah?', 'Batang Ventod'),
-(14, 5, 40, '2023-10-26 17:11:00', 'Sakit Hati', 'Booba'),
-(15, 10, 44, '2023-10-30 19:40:00', 'Sakit punggung', 'Kretek abal abal');
+(17, 5, 45, '2023-11-07 00:00:00', 'Sakit Hati', 'Dia'),
+(18, 5, 45, '2023-11-07 00:00:00', 'Sakit Panu', 'Saleb 88'),
+(19, 11, 46, '2023-11-09 00:30:00', 'Sakit Punggung', 'Kretek abal abal');
 
 -- --------------------------------------------------------
 
@@ -102,22 +99,24 @@ INSERT INTO `periksa` (`id`, `id_pasien`, `id_dokter`, `tgl_periksa`, `catatan`,
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` enum('dokter','pasien') NOT NULL
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
-(1, 'dokter', 'dokter', 'dokter'),
-(2, 'otto', '12345', 'dokter'),
-(3, 'pasien', 'pasien', 'pasien'),
-(4, 'peter', '12345', 'pasien'),
-(5, 'Kraven The Hunter', 'kravenchan123', 'pasien'),
-(6, 'Miles Morales', '12345', 'dokter'),
-(7, 'Morax', '12345', 'dokter');
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(11, 'gwen', '$2y$10$y.dLPPIeHrxlKVZDPGwI7eXj0zVIlsuKRjdolGwRxPyLQc2V2GyFO'),
+(12, 'peter', '$2y$10$jNvBBHWESpO4AVMGq8RC.O.NhUkHisIZUAlmLqTr.4dHBTrxbYtsq'),
+(13, 'Kraven The Hunter', '$2y$10$CqfhPwiGWKATyR3ABTwkvO3T8jEdMuTuT0YuKDUQxhj3bFVPo0DMK'),
+(14, 'Kraven', '$2y$10$XNxR81B7Xh2Mnqk2TRsNfeKNv9hkZ6WsTgD3xEBW/UdKuSHsCaj.m'),
+(15, 'Silk', '$2y$10$5S065ygD8KKQHk8seS1dIurnkxRcfj/5Sr0iqPiYMwUSr6ld6A9re'),
+(16, 'dokter', '$2y$10$PRVN.RtSv5QfQbrcl5oaJ.T7wN0g939QDu3itdQ.JfyU9mVkIrlU.'),
+(17, 'Cindy Moon', '$2y$10$7aZpJVgs/Sk99fCA6BOMLOy6Ya/9Sj8KF1PD/SlLv02mw6wB.QAWm'),
+(18, 'SpiderGwen', '$2y$10$ApcfhmFHUfv5pzTna95bP.SRhL41mGMcVw0GQQfh0wWQ8A4F8AwO2'),
+(19, 'Roja', '$2y$10$18Zq9C.Wh9v2cklEvC8IZ.V0aWrnQRUtfbrl91jBATKoSX6.xq75G'),
+(20, 'Roja28', '$2y$10$z6tvQk1vueBQL7rQrApeQu8gkL/5Xs7GM.mCgOUandXToJfGAJEUS');
 
 --
 -- Indexes for dumped tables
@@ -157,25 +156,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `dokter`
 --
 ALTER TABLE `dokter`
-  MODIFY `id_dokter` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id_dokter` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `pasien`
 --
 ALTER TABLE `pasien`
-  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `periksa`
 --
 ALTER TABLE `periksa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Constraints for dumped tables
